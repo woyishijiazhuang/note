806ec5b98a7f783da3a15413c76eeb39bae5ad28
woyishijiazhuang <44568291+woyishijiazhuang@users.noreply.github.com>
2023-08-16 01:29:57 +0800

    Create 循环请求动画帧.js
    完美

---
+++ 循环请求动画帧.js
@@ -0,0 +1,24 @@
+/**
+ * always requestAnimationFrame 连续请求动画帧
+ * 当你希望动画停止时，应当让callback返回true
+ * 或者调用该方法返回的函数
+ * @param {Function} callback
+ * @returns {Function}
+ * @example 
+ * // 在大多数设备上会运行60次callback
+ * let stopAlwasRAF = alwaysRAF(callback)
+ * setTimeout(stopAlwasRAF, 1000)
+ */
+function alwaysRAF(callback) {
+  let stop = null
+  
+  const loopRAF = callback => {
+    requestAnimationFrame(DOMHighResTimeStamp => {
+      if (stop || callback(DOMHighResTimeStamp)) return
+      loopRAF(callback)
+    })
+  }
+  loopRAF(callback)
+  // 返回一个停止该函数的方法
+  return () => stop = true
+}
